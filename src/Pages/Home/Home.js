ab899c73f8f17059109353f06917f6ba00d8278a
mehedi hasan <mehedicsebu@gmail.com>
2022-06-09 01:54:04 +0600

    daisyUI and tailwind for react added

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+const Home = () => {
+    return (
+        <div className='bg-red-300'>
+            <Navbar></Navbar>
+            <h3 className='bg-primary'>This Is Home Contenct</h3>
+        </div>
+    );
+};
+
+export default Home;
