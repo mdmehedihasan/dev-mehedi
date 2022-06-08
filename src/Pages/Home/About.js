7c7c46d455021f5baa495ed321322cd599b0aae3
mehedi hasan <mehedicsebu@gmail.com>
2022-06-09 03:49:13 +0600

    about me added

--- src/Pages/Home/About.js
+++ src/Pages/Home/About.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import mypic from '/src/assets/mehediprofile.png'
+
 
 const About = () => {
     return (
         <section>
-            <div class="hero min-h-screen bg-base-200">
-                <div class="hero-content flex-col lg:flex-row-reverse">
-                    <img src={mypic} class="max-w-sm rounded-lg shadow-2xl" />
+
+            <div className='bg-base-300 p-12 min-h-full'>
+                <h1 className='text-4xl text-left pb-4'>About Me</h1>
+                <div className='grid grid-cols-2 gap-10 items-center justify-center '>
                     <div>
-                        <h1 class="text-5xl font-bold">Box Office News!</h1>
-                        <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button class="btn btn-primary">Get Started</button>
+                        <div>
+                            <img className='object-right' src="https://rock-tyre.web.app/static/media/profile-1.0eab64be881f4976d04f.png" alt='name' />
+                        </div>
+                    </div>
+                    <div>
+                        <p class="py-6 text-justify text-2xl font-semi-bold">I am a WEB Developer. I am working with React, Node.js, and MongoDB.I also have experience in developing static websites using HTML, CSS and JavaScript(ES6).</p>
+                        <p class="py-6 text-justify text-2xl font-semi-bold">I am a WEB Developer. I am working with React, Node.js, and MongoDB.I also have experience in developing static websites using HTML, CSS and JavaScript(ES6).</p>
+
                     </div>
                 </div>
             </div>
