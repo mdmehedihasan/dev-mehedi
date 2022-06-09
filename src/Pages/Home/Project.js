c02bf0e48fa7b3b613c1ba981858a2ace3ddb5f7
mehedi hasan <mehedicsebu@gmail.com>
2022-06-09 19:20:23 +0600

    my projects updated

--- src/Pages/Home/Project.js
+++ src/Pages/Home/Project.js
@@ -1,27 +1,26 @@
 import React from 'react';
 import './Project.css'
-import { FaGithub, FaMapMarked, FaPhone, FaEnvelope } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa'
 
 const Project = ({ project }) => {
-    const { link, name, img, description, technology } = project;
+    const { live_link, name, img, description, technology, client_side, server_side } = project;
     return (
 
-        <div>
+        <div className='px-4'>
             <div>
                 <div style={{ backgroundImage: `url(${img})` }} className='img'>
-                    <a href={link} class="btn-visit">Visit</a>
+                    <a href={live_link} class="btn-visit">Visit</a>
 
                 </div>
             </div>
-            <div>
+            <div className='bg-slate-100 p-2'>
                 <div className='flex items-center justify-center'>
                     <div className='p-2 text-3xl'>
-                        <a href={link}> <FaGithub /> </a>
+                        <a href={client_side}> <FaGithub /> </a>
                     </div>
                     <div className='p-2 text-3xl'>
-                        <a href={link}> <FaGithub /> </a>
+                        <a href={server_side}> <FaGithub /> </a>
                     </div>
-
                 </div>
                 <h2 className='text-2xl font-semibold'>{name}</h2>
                 <p className='text-xl text-justify py-4'>{description}</p>
