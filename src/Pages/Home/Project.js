fa0d9b2f4c97106080c018e7a15ef9d54f6719f4
mehedi hasan <mehedicsebu@gmail.com>
2022-06-09 20:29:52 +0600

    project section added and max-w-lg set

--- src/Pages/Home/Project.js
+++ src/Pages/Home/Project.js
@@ -6,26 +6,28 @@ const Project = ({ project }) => {
     const { live_link, name, img, description, technology, client_side, server_side } = project;
     return (
 
-        <div className='flex flex-col items-center justify-center'>
-            <div style={{ backgroundImage: `url(${img})` }} className='img'>
-                <a href={live_link} class="btn-visit">Visit</a>
+        <section className='card lg:max-w-lg'>
+            <div className='flex flex-col items-center justify-center'>
+                <div style={{ backgroundImage: `url(${img})` }} className='img'>
+                    <a href={live_link} class="btn-visit">Visit</a>
 
-            </div>
-
-            <div className=' p-2'>
-                <div className='flex items-center justify-center'>
-                    <div className='p-2 text-3xl'>
-                        <a href={client_side}> <FaGithub /> </a>
-                    </div>
-                    <div className='p-2 text-3xl'>
-                        <a href={server_side}> <FaGithub /> </a>
-                    </div>
                 </div>
-                <h2 className='text-2xl font-semibold'>{name}</h2>
-                <p className='text-xl text-justify py-4'>{description}</p>
-                <p className='text-xl font-semibold text-justify py-4'>{technology}</p>
+
+                <div className=' p-2'>
+                    <div className='flex items-center justify-center'>
+                        <div className='p-2 text-3xl'>
+                            <a href={client_side}> <FaGithub /> </a>
+                        </div>
+                        <div className='p-2 text-3xl'>
+                            <a href={server_side}> <FaGithub /> </a>
+                        </div>
+                    </div>
+                    <h2 className='text-2xl font-semibold'>{name}</h2>
+                    <p className='text-xl text-justify py-4'>{description}</p>
+                    <p className='text-xl font-semibold text-justify py-4'>{technology}</p>
+                </div>
             </div>
-        </div>
+        </section>
 
     );
 };
