4b8c22f62d1c3d547845b2d697544a12a78eacd6
mehedi hasan <mehedicsebu@gmail.com>
2022-06-10 15:52:02 +0600

    footer color changed

--- src/Pages/Home/Footer.js
+++ src/Pages/Home/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <footer className='text-xl font-semibold row lg:mt-36 md:mt-20 sm:mt-16 xs:mt-10 lg:mb-16 md:mb-16sm:mb-16 px-4 '> @2022 allrights reserved by MEHEDI HASAN</footer>
+        <footer className='lg:text-xl lg:font-semibold row lg:mt-36 md:mt-20 sm:mt-16 xs:mt-10 lg:mb-16 md:mb-16sm:mb-16 px-4 text-sobuj '> @2022 ALL RIGHTS RESERVED BY MD MEHEDI HASAN</footer>
     );
 };
 
